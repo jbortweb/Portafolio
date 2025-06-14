b2d8dff23dfbc83b89e7b5649c7f2a0cd3344942
Jbortweb <jbortweb@gmail.com>
2025-06-14 17:52:21 +0200

    favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 export default defineNuxtConfig({
   app: {
     head: {
-      link: [{ rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }],
+      link: [{ rel: 'icon', type: 'image/svg+xml', href: '/favicon.ico' }],
     },
   },
   modules: ['@nuxtjs/tailwindcss'],
