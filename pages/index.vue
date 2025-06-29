f34fa7badb340a9a85bf7c45b73168df29c2b22e
Jbortweb <jbortweb@gmail.com>
2025-06-29 17:27:40 +0200

    feat: ajustar estilos de altura en la sección principal para mejorar la presentación

--- pages/index.vue
+++ pages/index.vue
@@ -526,7 +526,7 @@ onMounted(() => {
 
 <template>
   <main>
-    <div class="h-[1150dvh] sm:h-[1400dvh] md:h-[1020dvh]">
+    <div class="h-[1150dvh] lg:h-[1020dvh]">
       <div id="logo-mask" class="fixed top-0 w-full h-screen">
         <section id="seccion-1" class="h-screen">
           <picture
